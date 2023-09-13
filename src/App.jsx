f189b7667d5f2cfd0aafef807fc94452b01b7dd9
Dev-anshika98 <g.anshikacreative@gmail.com>
2023-09-14 01:17:53 +0530

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
+import Navbar from './Components/Navbar';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <h1 className="text-3xl font-bold text-blue-900 underline text-center">Hello world!</h1>
+   <>
+  <Navbar />
+   </>
   )
 }
 
-export default App
+export default App;
